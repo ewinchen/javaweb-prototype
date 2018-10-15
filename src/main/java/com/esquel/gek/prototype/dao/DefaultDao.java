2e25178a42e0e6d25edd37a3e6b8551f07fe87d8
ewinchen <chenyongp@esquel.com>
2018-10-15 16:50:10 +0800

    feat: integrate mybatis-generator

--- src/main/java/com/esquel/gek/prototype/dao/DefaultDao.java
+++ src/main/java/com/esquel/gek/prototype/dao/DefaultDao.java
@@ -18,14 +18,25 @@ public class DefaultDao {
     @Autowired
     private SqlSession sqlSessionTemplate;
 
-    public Users getUser(long id) {
-        return sqlSessionTemplate.selectOne("selectUserById", id);
-    }
-
+    /**
+     * 可以直接调用生成的Mapper 的方法，因为已为sqlSession 注入所有statement
+     * 没必要这样用，直接用对应的Mapper 就行了
+     * 这里只是测试，验证
+     */
     public Users getUserProxy(long id) {
         return sqlSessionTemplate.selectOne("selectById", id);
     }
 
+    /**
+     * 调用自定义Mapper 的方法
+     */
+    public Users getUser(long id) {
+        return sqlSessionTemplate.selectOne("selectUserById", id);
+    }
+
+    /**
+     * 调用另一个数据源的自定义Mapper 的方法
+     */
     public Map<String, Object> getCity() {
         return mysqlSqlSession.selectOne("selectCity");
     }
--- src/main/java/com/esquel/gek/prototype/service/impl/DefaultServiceImpl.java
+++ src/main/java/com/esquel/gek/prototype/service/impl/DefaultServiceImpl.java
@@ -1,19 +1,15 @@
 package com.esquel.gek.prototype.service.impl;
 
 import com.esquel.gek.prototype.dao.DefaultDao;
+import com.esquel.gek.prototype.domain.UsersExample;
 import com.esquel.gek.prototype.mapper.UsersMapper;
 import com.esquel.gek.prototype.service.DefaultService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.validation.constraints.Null;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -25,8 +21,9 @@ public class DefaultServiceImpl implements DefaultService {
     /**
      * 使用@Mapper，并且是单数据源的时候是不需要@Autowired的  参考日志：Enabling autowire by type for MapperFactoryBean with name 'usersMapper'.
      * 多数据源的时候，不加@AutoWired的话，系统启动时不知道注入到哪个SqlSession，会报错，加上@Autowired 会自动注入到默认的SqlSession！
-     *
+     * <p>
      * 如果不使用@Mapper的话，不会自动注入，须要@Autowired的注解！
+     * 但是代码里没有实现，所以须要用@SuppressWarnings
      */
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
@@ -35,14 +32,6 @@ public class DefaultServiceImpl implements DefaultService {
     @Autowired
     private DefaultDao defaultDao;
 
-//    @Autowired
-//    @Qualifier("kmisJdbcTemplate")
-//    private JdbcTemplate kmisJdbcTemplate;
-//
-//    @Autowired
-//    @Qualifier("uatJdbcTemplate")
-//    private JdbcTemplate uatJdbcTemplate;
-
     @Override
     public Map<String, Object> getGkNumInfo() {
         return null;
@@ -50,12 +39,6 @@ public class DefaultServiceImpl implements DefaultService {
 
     @Override
     public List<Map<String, Object>> getJobNum() {
-//        List<Map<String, Object>> a = uatJdbcTemplate.queryForList("SELECT TOP 10 * FROM ArtDB.dbo.rtPdInfo;");
-//        List<Map<String, Object>> b = kmisJdbcTemplate.queryForList("SELECT TOP 10 * FROM ArtDB.dbo.rtPdInfo;");
-//
-//        List<Map<String, Object>> res = new ArrayList<>();
-//        res.addAll(a);
-//        res.addAll(b);
         return null;
     }
 
@@ -77,16 +60,22 @@ public class DefaultServiceImpl implements DefaultService {
     @Override
     @Transactional
     public void hello() {
-        logger.info(usersMapper.findByUsername("admin").toString());
+        /**
+         * 测试使用Mapper
+         */
+        UsersExample usersExample = new UsersExample();
 
-        logger.info(usersMapper.selectById(1).toString());
+        usersExample.or().andUsernameEqualTo("admin");
 
-        logger.info(defaultDao.getUser(2).toString());
+        logger.info("Mapper of UsersExample: " + usersMapper.selectByExample(usersExample).get(0).toString());
 
-        logger.info(defaultDao.getCity().toString());
+        logger.info("Mapper of Primary Key: " + usersMapper.selectByPrimaryKey(1L).toString());
 
-        logger.info(defaultDao.getUserProxy(3).toString());
-
+        /**
+         * 测试调用不同的数据源
+         */
+        logger.info("Dao: " + defaultDao.getUser(2).toString());
+        logger.info("Dao of Mysql: " + defaultDao.getCity().toString());
     }
 
 
